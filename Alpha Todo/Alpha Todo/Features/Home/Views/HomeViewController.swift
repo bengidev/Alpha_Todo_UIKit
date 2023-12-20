8953ded65e535deb2080e4fdfca9f58d006ec3f5
B e n g i <bambang.trd17@gmail.com>
2023-12-20 15:06:53 +0700

    Add HomeViewController into Home feature

---
+++ Alpha Todo/Alpha Todo/Features/Home/Views/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  Alpha Todo
+//
+//  Created by Bambang Tri Rahmat Doni on 20/12/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        
+        self.view.backgroundColor = .red
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
