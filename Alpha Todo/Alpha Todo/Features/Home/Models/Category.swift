801369430bcce243ae6dec8795def6f0bd0b5215
B e n g i <bambang.trd17@gmail.com>
2023-12-21 18:05:10 +0700

    Add Category into Home Models folder

---
+++ Alpha Todo/Alpha Todo/Features/Home/Models/Category.swift
@@ -0,0 +1,29 @@
+//
+//  Category.swift
+//  Alpha Todo
+//
+//  Created by Bambang Tri Rahmat Doni on 21/12/23.
+//
+
+import UIKit
+
+struct Category: Hashable, Identifiable {
+    let id = UUID()
+    var name: String
+    var imageName: String
+    var isSelected: Bool
+    
+    static let empty: Category = .init(name: "", imageName: "", isSelected: false)
+    
+    mutating func changeName(_ name: String) -> Void {
+        self.name = name
+    }
+    
+    mutating func changeImageName(_ imageName: String) -> Void {
+        self.imageName = imageName
+    }
+    
+    mutating func changeIsSelected(_ isSelected: Bool) -> Void {
+        self.isSelected = isSelected
+    }
+}
