3c5d95e0e829520655fd2e295820942f50c89074
B e n g i <bambang.trd17@gmail.com>
2023-12-23 12:00:38 +0700

    Separate HomeView from HomeViewController

--- Alpha Todo/Alpha Todo/Features/Home/Views/HomeViewController.swift
+++ Alpha Todo/Alpha Todo/Features/Home/Views/HomeViewController.swift
@@ -11,80 +11,35 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     // MARK: Properties
-    private let viewModel = HomeViewModel()
-    
+    private var homeView = HomeView()
     private var selectedCategory: Set<Category> = []
     
-    // MARK: View Components
-    private lazy var containerVStackView: UIStackView = {
-        let vw = AppViewFactory.buildStackView()
-        vw.axis = .vertical
-        vw.backgroundColor = .red
-        return vw
-    }()
+    private let homeViewModel = HomeViewModel()
     
-    private lazy var oneHStackView: UIStackView = {
-        let vw = AppViewFactory.buildStackView()
-        vw.axis = .horizontal
-        vw.backgroundColor = .blue
-        return vw
-    }()
     
-    private lazy var oneVStackView: UIStackView = {
-        let vw = AppViewFactory.buildStackView()
-        vw.axis = .vertical
+    
+    // MARK: Initializers
+    init() { super.init(nibName: nil, bundle: nil) }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
         
-        return vw
-    }()
+        fatalError("init(coder:) has not been implemented")
+    }
     
-    private lazy var helloLabel: UILabel = {
-        let lb = AppViewFactory.buildLabel()
-        lb.textAlignment = .left
-        lb.text = "Hello,"
-        lb.font = .preferredFont(forTextStyle: .largeTitle).rounded()
-        lb.textColor = .gray
+    @available(*, unavailable)
+    override class func awakeFromNib() {
+        super.awakeFromNib()
         
-        return lb
-    }()
-    
-    private lazy var nameLabel: UILabel = {
-        let lb = AppViewFactory.buildLabel()
-        lb.textAlignment = .left
-        lb.text = "Lorem Ipsum Torum"
-        lb.font = .preferredFont(forTextStyle: .title1).bold().rounded()
-        lb.textColor = .black
-        
-        return lb
-    }()
-    
-    private lazy var profileImageView: UIImageView = {
-        let vw = AppViewFactory.imageView()
-        vw.image = .init(named: "Building")
-        vw.layer.cornerRadius = 10.0
-        vw.clipsToBounds = true
-        
-        return vw
-    }()
-    
-    private lazy var categoryCollectionView: UICollectionView = {
-        let vw = AppViewFactory.buildCollectionView(scrollDirection: .horizontal)
-        vw.dataSource = self
-        vw.delegate = self
-        vw.register(
-            CategoryCollectionViewCell.self,
-            forCellWithReuseIdentifier: CategoryCollectionViewCell.identifier
-        )
-        
-        return vw
-    }()
+        fatalError("awakeFromNib() has not been implemented")
+    }
     
     // MARK: Lifecycles
     override func loadView() {
         super.loadView()
         
-        self.view.backgroundColor = .appSecondary
         self.setupViews()
-        
     }
     
     override func viewDidLoad() {
@@ -93,6 +48,7 @@ final class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // MARK: Functionalities
     @objc
     private func didTapCategoryButton(_ sender: UIButton) -> Void {
         // Retrieve the indexPath using the tag
@@ -102,42 +58,21 @@ final class HomeViewController: UIViewController {
         print("Category Button tapped in cell at indexPath: \(indexPath)")
     }
     
-    // MARK: Functionalities
     private func setupViews() -> Void {
-        self.view.addSubview(self.containerVStackView)
+        self.view = self.homeView
         
-        self.containerVStackView.addArrangedSubview(self.oneHStackView)
-        self.containerVStackView.addArrangedSubview(self.categoryCollectionView)
-        self.containerVStackView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.edges.equalToSuperview()
-        }
+        self.setupCategoryCollectionController()
+    }
+    
+    private func setupCategoryCollectionController() -> Void {
+        let categoryCollectionController = CategoryCollectionViewController(
+            categories: self.homeViewModel.categories
+        )
         
-        self.oneHStackView.addArrangedSubview(self.oneVStackView)
-        self.oneHStackView.addArrangedSubview(self.self.profileImageView)
-        self.oneHStackView.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(10.0)
-        }
+        // Include that child view controller in the parent's view controller life cycle.
+        self.add(categoryCollectionController)
         
-        self.profileImageView.snp.makeConstraints { make in
-            make.width.height.equalTo(100.0)
-        }
-        
-        self.oneVStackView.addArrangedSubview(self.helloLabel)
-        self.oneVStackView.addArrangedSubview(self.nameLabel)
-        
-        self.helloLabel.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview()
-        }
-        
-        self.nameLabel.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview()
-        }
-        
-        self.categoryCollectionView.snp.makeConstraints { make in
-            make.height.equalTo(65.0)
-            make.horizontalEdges.equalToSuperview().inset(10.0)
-        }
+        self.homeView.updateCategoryCollectionController(categoryCollectionController)
     }
 }
 
@@ -147,7 +82,7 @@ extension HomeViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.viewModel.categories.count
+        return self.homeViewModel.categories.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -157,7 +92,7 @@ extension HomeViewController: UICollectionViewDataSource {
         ) as? CategoryCollectionViewCell else { return .init() }
         
         
-        cell.updateCategoryButton(with: self.viewModel.categories[indexPath.item])
+        cell.updateCategoryButton(with: self.homeViewModel.categories[indexPath.item])
         let categoryButton = cell.getCategoryButton()
         categoryButton.tag = indexPath.item
         categoryButton.addTarget(self, action: #selector(self.didTapCategoryButton(_:)), for: .touchUpInside)
