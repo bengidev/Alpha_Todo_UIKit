5786faa6965d951d659d6822849f504bd766fe4d
B e n g i <bambang.trd17@gmail.com>
2023-12-23 09:32:38 +0700

    Add comments inside UIViewController+Extension

--- Alpha Todo/Alpha Todo/Extensions/UIViewController+Extension.swift
+++ Alpha Todo/Alpha Todo/Extensions/UIViewController+Extension.swift
@@ -8,14 +8,23 @@
 import UIKit
 
 extension UIViewController {
-    // Add another UIViewController to be child of self
+    /// When you extract child view controller from a parent, there are three things you need to do
+    /// in order to include that child view controller in the parent's view controller life cycle.
+    /// 
+    /// First, add the view of the child to the view of the parent.
+    /// Then, add the child to the parent.
+    /// Finally, notify the child that it was moved to a parent.
+    ///
+    /// - Parameter child: Another UIViewController which will be the child.
+    ///
     func add(_ child: UIViewController) {
         addChild(child)
         view.addSubview(child.view)
         child.didMove(toParent: self)
     }
 
-    // Remove child UIViewController from self if available
+    /// Remove the child UIViewController from parent UIViewController if available.
+    ///
     func remove() {
         guard parent != nil else {
             return
