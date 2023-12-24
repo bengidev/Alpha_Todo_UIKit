7848cfb6a6b0bf44fc7aa2cd9ecb5ee41a0faf98
B e n g i <bambang.trd17@gmail.com>
2023-12-24 16:43:36 +0700

    Combine Category and Todo into Tasks

--- Alpha Todo/Alpha Todo/Features/Home/ViewModels/HomeViewModel.swift
+++ Alpha Todo/Alpha Todo/Features/Home/ViewModels/HomeViewModel.swift
@@ -8,13 +8,48 @@
 import Foundation
 
 final class HomeViewModel {
-    private(set) var categories: [Category] = [
-        .init(name: "Personal", imageName: "person.fill", isSelected: false),
-        .init(name: "Work", imageName: "tv.fill", isSelected: false),
-        .init(name: "Health", imageName: "heart.fill", isSelected: false),
-        .init(name: "Finance", imageName: "dollarsign.circle.fill", isSelected: false),
-        .init(name: "Study", imageName: "book.fill", isSelected: false),
-        .init(name: "Social", imageName: "person.3.fill", isSelected: false),
+    private(set) var tasks: [Task] = [
+        .init(
+            category: .init(name: "Personal", imageName: "person.fill", isSelected: false),
+            todos: [
+                .init(
+                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+                timeStart: .init(),
+                timeEnd: .init(),
+                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
+                isImportant: true,
+                hasCompleted: false
+            ),
+                .init(
+                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+                timeStart: .init(),
+                timeEnd: .init(),
+                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
+                isImportant: true,
+                hasCompleted: false
+            ),
+                .init(
+                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+                timeStart: .init(),
+                timeEnd: .init(),
+                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
+                isImportant: true,
+                hasCompleted: false
+            ),
+            ]
+        ),
+        .init(
+            category: .init(name: "Work", imageName: "tv.fill", isSelected: false),
+            todos: [.init(
+                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+                timeStart: .init(),
+                timeEnd: .init(),
+                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
+                isImportant: true,
+                hasCompleted: false
+            ),
+            ]
+        ),
     ]
     
 }
