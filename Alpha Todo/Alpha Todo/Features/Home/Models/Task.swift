efbc073c27f7646d448da60f01c486ae5272ee00
B e n g i <bambang.trd17@gmail.com>
2023-12-24 16:42:37 +0700

    Add Task model into Home feature

---
+++ Alpha Todo/Alpha Todo/Features/Home/Models/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  Alpha Todo
+//
+//  Created by Bambang Tri Rahmat Doni on 24/12/23.
+//
+
+import Foundation
+
+struct Task: Hashable, Identifiable {
+    let id = UUID()
+    var category: Category
+    var todos: [Todo]
+    
+    static let empty: Task = .init(category: .empty, todos: [.empty])
+}
