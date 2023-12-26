c0b243a2550f0c4c2eab0df10e225e475e79e9da
B e n g i <bambang.trd17@gmail.com>
2023-12-26 18:03:40 +0700

    fix: add functionalities for managing data Tasks

--- Alpha Todo/Alpha Todo/Features/Home/ViewModels/HomeViewModel.swift
+++ Alpha Todo/Alpha Todo/Features/Home/ViewModels/HomeViewModel.swift
@@ -9,53 +9,16 @@ import Foundation
 
 @MainActor
 final class HomeViewModel {
-    private(set) var tasks: [Task] = [
-        .init(
-            category: .init(name: "Personal", imageName: "person.fill", isSelected: false),
-            todos: [
-                .init(
-                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                timeStart: .init(),
-                timeEnd: .init(),
-                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
-                isImportant: true,
-                hasCompleted: false
-            ),
-                .init(
-                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                timeStart: .init(),
-                timeEnd: .init(),
-                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
-                isImportant: false,
-                hasCompleted: false
-            ),
-                .init(
-                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                timeStart: .init(),
-                timeEnd: .init(),
-                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
-                isImportant: true,
-                hasCompleted: true
-            ),
-            ]
-        ),
-        .init(
-            category: .init(name: "Work", imageName: "tv.fill", isSelected: false),
-            todos: [.init(
-                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                timeStart: .init(),
-                timeEnd: .init(),
-                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
-                isImportant: false,
-                hasCompleted: false
-            ),
-            ]
-        ),
-    ]
-    
-    private(set) var emptyTasks: [Task] = []
+    private(set) var tasks: [Task] = []
     
     func addNewTask(_ task: Task) -> Void {
         self.tasks.append(task)
+    }
+    
+    func updateCurrentTask(for indexPath: IndexPath, with todos: [Todo]) -> Void {
+        for todo in todos {
+            self.tasks[indexPath.row].addNewTodo(todo)
+        }
+        
     }
 }
