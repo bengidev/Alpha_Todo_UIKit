24a915f9442dc763c10cecfb5a847053cd17f438
B e n g i <bambang.trd17@gmail.com>
2023-12-26 14:41:19 +0700

    fix: add extension for Notification.Name

---
+++ Alpha Todo/Alpha Todo/Extensions/NotificationCenter+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationCenter+Extension.swift
+//  Alpha Todo
+//
+//  Created by Bambang Tri Rahmat Doni on 26/12/23.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let TaskDidTapSaveButton = Notification.Name("TaskDidTapSaveButton")
+    static let HomeDidTapAddButton = Notification.Name("HomeDidTapAddButton")
+}
