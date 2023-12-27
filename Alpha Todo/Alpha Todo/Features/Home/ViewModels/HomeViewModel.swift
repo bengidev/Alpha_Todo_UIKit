2fe8b767dd2062ab1d4964dd2cfa0b0b08c48a8f
B e n g i <bambang.trd17@gmail.com>
2023-12-27 07:09:58 +0700

    fix: add dummy data Tasks for testing purpose

--- Alpha Todo/Alpha Todo/Features/Home/ViewModels/HomeViewModel.swift
+++ Alpha Todo/Alpha Todo/Features/Home/ViewModels/HomeViewModel.swift
@@ -9,7 +9,49 @@ import Foundation
 
 @MainActor
 final class HomeViewModel {
-    private(set) var tasks: [Task] = []
+    private(set) var tasks: [Task] = [
+        .init(
+            category: .init(name: "Personal", imageName: "person.fill", isSelected: false),
+            todos: [
+                .init(
+                    title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+                    timeStart: .init(),
+                    timeEnd: .init(),
+                    description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
+                    isImportant: true,
+                    hasCompleted: false
+                ),
+                .init(
+                    title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+                    timeStart: .init(),
+                    timeEnd: .init(),
+                    description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
+                    isImportant: false,
+                    hasCompleted: false
+                ),
+                .init(
+                    title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+                    timeStart: .init(),
+                    timeEnd: .init(),
+                    description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
+                    isImportant: true,
+                    hasCompleted: true
+                ),
+            ]
+        ),
+        .init(
+            category: .init(name: "Work", imageName: "tv.fill", isSelected: false),
+            todos: [.init(
+                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+                timeStart: .init(),
+                timeEnd: .init(),
+                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
+                isImportant: false,
+                hasCompleted: false
+            ),
+            ]
+        ),
+    ]
     
     func addNewTask(_ task: Task) -> Void {
         self.tasks.append(task)
