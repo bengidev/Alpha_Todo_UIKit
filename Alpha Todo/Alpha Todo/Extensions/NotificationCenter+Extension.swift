923c642100ab38c3b05750a63c020fb15c07c566
B e n g i <bambang.trd17@gmail.com>
2023-12-27 12:59:18 +0700

    refactor: add new name into Notification.Name

--- Alpha Todo/Alpha Todo/Extensions/NotificationCenter+Extension.swift
+++ Alpha Todo/Alpha Todo/Extensions/NotificationCenter+Extension.swift
@@ -13,4 +13,5 @@ extension Notification.Name {
     static let TodoDataTaskChanged = Notification.Name("TodoDataTaskChanged")
     static let TaskDidTapSaveButton = Notification.Name("TaskDidTapSaveButton")
     static let CategoryDidTapCategoryButton = Notification.Name("CategoryDidTapCategoryButton")
+    static let CategoryDataTasksChanged = Notification.Name("CategoryDataTasksChanged")
 }
