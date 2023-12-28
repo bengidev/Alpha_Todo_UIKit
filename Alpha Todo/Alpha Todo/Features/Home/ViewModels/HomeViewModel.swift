af314235b12e818401af43b4ed5f29cf86ae5bdf
B e n g i <bambang.trd17@gmail.com>
2023-12-28 13:00:36 +0700

    refactor: remove dummy data Tasks

--- Alpha Todo/Alpha Todo/Features/Home/ViewModels/HomeViewModel.swift
+++ Alpha Todo/Alpha Todo/Features/Home/ViewModels/HomeViewModel.swift
@@ -9,55 +9,33 @@ import Foundation
 
 @MainActor
 final class HomeViewModel {
-    private(set) var tasks: [Task] = [
-        .init(
-            category: .init(name: "Personal", imageName: "person.fill", isSelected: false),
-            todos: [
-                .init(
-                    title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                    timeStart: .init(),
-                    timeEnd: .init(),
-                    description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
-                    isImportant: true,
-                    hasCompleted: false
-                ),
-                .init(
-                    title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                    timeStart: .init(),
-                    timeEnd: .init(),
-                    description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
-                    isImportant: false,
-                    hasCompleted: false
-                ),
-                .init(
-                    title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                    timeStart: .init(),
-                    timeEnd: .init(),
-                    description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
-                    isImportant: true,
-                    hasCompleted: true
-                ),
-            ]
-        ),
-        .init(
-            category: .init(name: "Work", imageName: "tv.fill", isSelected: false),
-            todos: [.init(
-                title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
-                timeStart: .init(),
-                timeEnd: .init(),
-                description: "Duis non odio arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum feugiat neque vitae nisl mattis, quis efficitur libero gravida. Quisque faucibus magna eu hendrerit placerat. Mauris laoreet dictum nisl, quis vestibulum magna. Proin vehicula, nulla at aliquam efficitur, nibh dui fringilla erat, pretium aliquam odio tellus maximus augue. Donec malesuada odio at neque sollicitudin, id cursus mauris euismod.",
-                isImportant: false,
-                hasCompleted: false
-            ),
-            ]
-        ),
+    private(set) var tasks: [Task] = []
+    private(set) var systemImageNames: [String] = [
+        "person.fill",
+        "house.fill",
+        "music.note.list",
+        "desktopcomputer",
+        "car.fill",
+        "heart.circle.fill",
+        "cart.fill",
+        "play.rectangle.fill",
+        "cloud.sun.rain.fill",
     ]
     
     func addNewTask(_ task: Task) -> Void {
-        self.tasks.append(task)
+        var tempTask = task
+        tempTask.category.imageName = self.randomSystemName()
+        
+        print("Temp Task: \(tempTask)")
+        
+        self.tasks.append(tempTask)
     }
     
     func updateCurrentTask(for indexPath: IndexPath, with todos: [Todo]) -> Void {
         self.tasks[indexPath.row].addNewTodos(todos)
+    }
+    
+    func randomSystemName() -> String {
+        return self.systemImageNames.randomElement() ?? "person.fill"
     }
 }
