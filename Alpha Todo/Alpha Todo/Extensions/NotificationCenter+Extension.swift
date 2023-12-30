1e20ef7fb204bebbd884c2e3ca0c7ef133ad53ca
B e n g i <bambang.trd17@gmail.com>
2023-12-30 11:40:30 +0700

    refactor: add another Notification names

--- Alpha Todo/Alpha Todo/Extensions/NotificationCenter+Extension.swift
+++ Alpha Todo/Alpha Todo/Extensions/NotificationCenter+Extension.swift
@@ -10,6 +10,7 @@ import Foundation
 extension Notification.Name {
     static let HomeSelectedTask = Notification.Name("HomeSelectedTask")
     static let HomeDidTapAddButton = Notification.Name("HomeDidTapAddButton")
+    static let HomeDidTapEditTodoButton = Notification.Name("HomeDidTapEditTodoButton")
     static let TodoSelectedTaskChanged = Notification.Name("TodoDataTaskChanged")
     static let TodoSelectedCategoryIndexPathChanged = Notification.Name("TodoSelectedCategoryIndexPathChanged")
     static let TaskDidTapSaveButton = Notification.Name("TaskDidTapSaveButton")
