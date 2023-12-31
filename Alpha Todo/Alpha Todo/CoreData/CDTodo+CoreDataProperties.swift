b64b7bb74293b143eaefa16b8c669929336dccc5
B e n g i <bambang.trd17@gmail.com>
2023-12-31 13:39:26 +0700

    refactor: add formattedDate to proper show Date

--- Alpha Todo/Alpha Todo/CoreData/CDTodo+CoreDataProperties.swift
+++ Alpha Todo/Alpha Todo/CoreData/CDTodo+CoreDataProperties.swift
@@ -11,24 +11,32 @@ import CoreData
 
 
 extension CDTodo {
-
+    
     @nonobjc public class func fetchRequest() -> NSFetchRequest<CDTodo> {
         return NSFetchRequest<CDTodo>(entityName: "CDTodo")
     }
-
+    
     @NSManaged public var descriptions: String?
     @NSManaged public var dueDate: Date?
     @NSManaged public var isCompleted: Bool
     @NSManaged public var isImportant: Bool
     @NSManaged public var title: String?
     @NSManaged public var task: CDAlphaTask?
-
+    
     var wrappedTitle: String {
         return self.title ?? ""
     }
     
     var wrappedDueDate: Date {
         return self.dueDate ?? .init()
+    }
+    
+    var formattedDate: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, yyyy 'at' hh:mm a"
+        formatter.locale = .current
+        
+        return formatter.string(from: self.wrappedDueDate)
     }
     
     var wrappedDescriptions: String {
@@ -42,9 +50,8 @@ extension CDTodo {
     var wrappedIsCompleted: Bool {
         return self.isCompleted
     }
-    
 }
 
 extension CDTodo : Identifiable {
-
+    
 }
