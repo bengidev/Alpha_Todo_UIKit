89fdd907399a0dc82cda863f37906a68a9e5ecee
B e n g i <bambang.trd17@gmail.com>
2024-01-02 12:48:41 +0700

    refactor: remove unnecessary function calls

--- Alpha Todo/Alpha Todo/Application/SceneDelegate.swift
+++ Alpha Todo/Alpha Todo/Application/SceneDelegate.swift
@@ -53,8 +53,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        
-        self.homeViewModel.fetchTasks()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
