3a7d4e159818f32aa4745957b1ce37375f47da60
B e n g i <bambang.trd17@gmail.com>
2024-01-02 09:06:54 +0700

    fix: save CoreData when scene state change

--- Alpha Todo/Alpha Todo/Application/SceneDelegate.swift
+++ Alpha Todo/Alpha Todo/Application/SceneDelegate.swift
@@ -11,6 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private let homeViewModel = HomeViewModel()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -52,12 +53,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        
+        self.homeViewModel.fetchTasks()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        
+        do { try self.homeViewModel.getContext.save() }
+        catch { print("SceneDelegate Error: \(error.localizedDescription)") }
     }
 
 
